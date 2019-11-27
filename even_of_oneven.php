b5f4d8d68fbcd71b520c656c32bba54c8b810e6d
ilias atrar <23261@ma-web.nl>
2019-11-27 11:50:48 +0100

    Uitwerking van de exercise

---
+++ even_of_oneven.php
@@ -0,0 +1,14 @@
+<?php
+$var;
+echo "Vul een getal in";
+$var1 = readline();
+$result = $var1 % 2;
+if (!$result)
+{
+  echo 'Dit is een even getal';
+}
+else
+{
+  echo 'Dit is een oneven getal';
+}
+ ?>
